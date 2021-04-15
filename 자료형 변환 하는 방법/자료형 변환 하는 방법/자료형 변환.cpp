baee1e822691d88e7e5b99594f563de3066f3e64
park <pocket42@naver.com>
2021-04-16 03:17:43 +0900

    Commit

---
+++ 자료형 변환 하는 방법/자료형 변환 하는 방법/자료형 변환.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a = 3.141592;
+	cout << a << endl;
+
+	//강제적으로 데이터형 변환
+	//typeName(a) (typeName)a
+	char ch = 'M';
+	cout << (int)ch << " " << int(ch) << endl;
+
+	//C++
+	//static_cast<typeName>
+
+	cout << static_cast<int>(ch) << endl;
+
+	return 0;
+}
